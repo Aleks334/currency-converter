843d0e8bbbe58e65575c9937827f47749ec7127f
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2023-11-07 18:30:52 +0100

    refactor: change type of date to pl format

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,24 @@
 import React, { useEffect, useState } from "react";
 
 export default function Footer() {
-  const [date, setDate] = useState(new Date());
+	const [date, setDate] = useState(new Date());
 
-  useEffect(() => {
-    setInterval(() => setDate(new Date()), 45000);
-  }, []);
-  return (
-    <footer className="main-footer">
-      <p className="current-date">
-        {date.toLocaleDateString("en-US", {
-          weekday: "long",
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-          hour: "numeric",
-          minute: "numeric",
-        })}
-      </p>
-    </footer>
-  );
+	useEffect(() => {
+		setInterval(() => setDate(new Date()), 1000);
+	}, []);
+
+	return (
+		<footer className="main-footer">
+			<p className="current-date">
+				{date.toLocaleDateString("pl-PL", {
+					day: "numeric",
+					month: "numeric",
+					year: "numeric",
+					hour: "numeric",
+					minute: "numeric",
+					second: "numeric",
+				})}
+			</p>
+		</footer>
+	);
 }
